c6e48296b4d25bbe14bf5c79220fb9c7e78e2975
chrisbangun <adichrisbangun18@gmail.com>
2017-02-12 03:04:03 +0700

    adding more evaluation functions

--- p2-Isolation/game_agent.py
+++ p2-Isolation/game_agent.py
@@ -6,12 +6,127 @@ augment the test suite with your own test cases to further test your code.
 You must test your agent's strength against a set of agents with known
 relative strength using tournament.py and include the results in your report.
 """
+import sys
+import math
 import random
-
 
 class Timeout(Exception):
     """Subclass base exception for code clarity."""
     pass
+
+
+def position_value(game, player):
+    """
+
+    :param game:
+    :param player:
+    :return:
+    """
+    def distance_to_center(_player):
+        player_location = game.get_player_location(_player)
+        center_x = (game.width - 1) / 2
+        center_y = (game.height - 1) / 2
+        return math.sqrt(math.pow(center_x - player_location[0], 2) + math.pow(center_y - player_location[1], 2))
+
+    my_position = len(game.get_legal_moves(player)) - distance_to_center(player)
+    opp_position = len(game.get_legal_moves(game.get_opponent(player))) - distance_to_center(game.get_opponent(player))
+
+    if my_position > opp_position:
+        return (3 * my_position / 2) - (opp_position / 2)
+    return (my_position / 2) - (3 * opp_position / 2)
+
+
+def longest_path(game, player):
+    """
+    count max number of moves a player has
+    assume other player doesn't move
+    :param game:
+    :param player:
+    :return:
+    """
+
+    def longest_path_internal(board, _player):
+        longest = 0
+        legal_moves = board.get_legal_moves(_player)
+        for move in legal_moves:
+            new_board = board.forecast_move(move)
+            path_length = longest_path_internal(new_board, _player) + 1
+            if path_length > longest:
+                longest = path_length
+            if longest > 10:
+                break
+        return longest
+
+    player1_longest_path = longest_path_internal(game, player)
+    player2_longest_path = longest_path_internal(game, game.get_opponent(player))
+
+    if player1_longest_path <= player2_longest_path or player1_longest_path == 0:
+        return float("-inf")
+    return float("inf")
+
+
+def get_open_area(game, player):
+
+    def get_surrounding_space(__space):
+        row = __space[0]
+        col = __space[1]
+        lower_row = row - 1
+        higher_row = row + 1
+        lower_col = col - 1
+        higher_col = col + 1
+        surrounding_spaces = []
+        if row > 0:
+            surrounding_spaces.append(tuple([lower_row, col]))
+            if col > 0:
+                surrounding_spaces.append(tuple([lower_row, lower_col]))
+                surrounding_spaces.append(tuple([row, lower_col]))
+            if col < game.width:
+                surrounding_spaces.append(tuple([lower_row, higher_col]))
+                surrounding_spaces.append(tuple([row, higher_col]))
+
+        if row < game.height:
+            surrounding_spaces.append(tuple([higher_row, col]))
+            if col > 0:
+                surrounding_spaces.append(tuple([higher_row, lower_col]))
+            if col < game.width:
+                surrounding_spaces.append(tuple([higher_row, higher_col]))
+
+        return surrounding_spaces
+
+    initial_loc = game.get_player_location(player)
+    to_examine = [initial_loc]
+    seen = set()
+    accessible = set()
+    blank_spaces = game.get_blank_spaces()
+    while len(to_examine) > 0:
+        space = to_examine.pop(0)
+        for _space in get_surrounding_space(space):
+            if _space in seen:
+                continue
+            seen.add(_space)
+            if _space in blank_spaces:
+                accessible.add(_space)
+                to_examine.append(_space)
+    return accessible
+
+
+def mid_game_eval(game, player):
+
+    def center_score(player_loc):
+        center = game.width / 2
+        return - abs(player_loc[0] - center) - abs(player_loc[1] - center)
+
+    my_cords = game.get_player_location(player)
+    opp_cords = game.get_player_location(game.get_opponent(player))
+
+    height = abs(my_cords[0] - opp_cords[0])
+    width = abs(my_cords[1] - opp_cords[1])
+
+    my_legal_moves = len(game.get_legal_moves(player))
+    opp_legal_moves = len(game.get_legal_moves(game.get_opponent(player)))
+    mobility_score = my_legal_moves - opp_legal_moves
+    proximity_to_center = center_score(my_cords) - center_score(opp_cords)
+    return 3 * mobility_score + proximity_to_center
 
 
 def custom_score(game, player):
@@ -38,84 +153,46 @@ def custom_score(game, player):
         The heuristic value of the current game state to the specified player.
     """
 
-    blank_spaces_left = len(game.get_blank_spaces())
-
-    def number_of_moves():
-        """
-        heuristic method used in the beginning of the game
-        simply count the number of legal moves
-        :return:
-        """
-        if game.is_loser(player):
-            return float("-inf")
-        if game.is_winner(player):
-            return float("inf")
-        return float(len(game.get_legal_moves(player)))
-
-    def open_area():
-        """
-        heuristic method used in the middle of the game
-        return the diff between the number of legal moves
-        for both players
-        :return:
-        """
-        if game.is_loser(player):
-            return float("-inf")
-
-        if game.is_winner(player):
-            return float("inf")
-
-        my_moves = len(game.get_legal_moves(player))
-        opp_moves = len(game.get_legal_moves(game.get_opponent(player)))
-
-        return float(my_moves - opp_moves)
-
-    def longest_path():
-        """
-        heuristic method when reaching the end of game
-        this method computationally more expensive from the previous two heuristics
-        :return:
-        """
-        def longest_path_internal(board, _player):
-            longest = 0
-            for move in board.get_legal_moves(_player):
-                new_board = board.forecast_move(move)
-                path_length = longest_path_internal(new_board, _player) + 1
-                if path_length > longest:
-                    longest = path_length
-                if longest > 20:  # try to avoid going more than 20 moves deep
-                    break
-            return float(longest)
-
-        if game.is_loser(player):
-            return float("-inf")
-
-        if game.is_winner(player):
-            return float("inf")
-
-        player1_longest_path = longest_path_internal(game, player)
-        player2_longest_path = longest_path_internal(game, game.get_opponent(player))
-
-        # it is player1 turn,
-        # if both player have the same longest path
-        # or player1 longest path is 0
-        # return -inf, which means player1 is lost
-        if player1_longest_path == player2_longest_path or player1_longest_path == 0:
-            return float("-inf")
-
-        if player1_longest_path > player2_longest_path:
-            return float("inf")
+    if game.is_loser(player):
         return float("-inf")
 
-    if blank_spaces_left < 20:
-        # End Game
-        return longest_path()
-    elif blank_spaces_left <= 42:
-        # Middle Game
-        return open_area()
-    else:
-        # Beginning Game
-        return number_of_moves()
+    if game.is_winner(player):
+        return float("inf")
+
+    return mid_game_eval(game, player)
+    # board_size = game.width * game.height
+    # blank_spaces_left = len(game.get_blank_spaces())
+    # if blank_spaces_left < 40:
+    # if blank_spaces_left <= 10:
+    #     return longest_path(game, player)
+
+    #     my_open_area = get_open_area(game, player)
+    #     opp_open_area = get_open_area(game, game.get_opponent(player))
+    #     if len(my_open_area) == 0:
+    #         return float("-inf")
+    #     if len(opp_open_area) == 0:
+    #         return float("inf")
+    #     if not my_open_area.intersection(opp_open_area):
+    #         return longest_path(game, player)
+    #     return mid_game_eval(game, player)
+        # float(len(game.get_legal_moves(player)) - len(game.get_legal_moves(
+        # game.get_opponent(player))))
+        # return mid_game_eval(game, player)
+    # return mid_game_eval(game, player)
+    # else:
+        # my_open_area = get_open_area(game, player)
+        # opp_open_area = get_open_area(game, game.get_opponent(player))
+        # if len(my_open_area) == 0:
+        #     return float("-inf")
+        # if len(opp_open_area) == 0:
+        #     return float("inf")
+        # if not my_open_area.intersection(opp_open_area):
+        #     if len(my_open_area) == len(opp_open_area):
+        #         return float("inf") if game.active_player == player else float("-inf")
+        #     return float("inf") if len(my_open_area) > len(opp_open_area) else float("-inf")
+        # return float(len(game.get_legal_moves(player)) - len(game.get_legal_moves(game.get_opponent(player))))
+
+
 
 
 class CustomPlayer:
@@ -205,7 +282,6 @@ class CustomPlayer:
         if not legal_moves:
             return best_move
 
-        depth = 0
         # Try to get the search method (minimax or alphabeta)
         try:
             search_method = getattr(self, self.method)
@@ -214,21 +290,19 @@ class CustomPlayer:
                                       format(self.__class__.__name__, self.method))
 
         if not self.iterative:
-            return search_method(game, depth+1)[1]
+            return search_method(game, self.search_depth)[1]
 
         try:
             # The search method call (alpha beta or minimax) should happen in
             # here in order to avoid timeout. The try/except block will
             # automatically catch the exception raised by the search method
             # when the timer gets close to expiring
-            while True:
-                best_score = -float("inf")
+            best_score = float("-inf")
+            for depth in range(1, sys.maxsize**10):
                 score, move = search_method(game, depth)
                 if score > best_score:
                     best_score = score
                     best_move = move
-                depth += 1
-
         except Timeout:
             # Handle any actions required at timeout, if necessary
             return best_move
@@ -272,14 +346,15 @@ class CustomPlayer:
                 player = game.active_player
             else:
                 player = game.inactive_player
-            return self.score(game, player), None
+            return self.score(game, player), (-1, -1)
 
         best_move = (-1, -1)
         legal_moves = game.get_legal_moves()
         if maximizing_player:
-            v = -float("inf")
+            v = float("-inf")
             for move in legal_moves:
-                score = self.minimax(game.forecast_move(move), depth - 1, False)[0]
+                next_game = game.forecast_move(move)
+                score = self.minimax(next_game, depth - 1, False)[0]
                 if score > v:
                     v = score
                     best_move = move
@@ -287,7 +362,8 @@ class CustomPlayer:
         else:
             v = float("inf")
             for move in legal_moves:
-                score = self.minimax(game.forecast_move(move), depth - 1, True)[0]
+                next_game = game.forecast_move(move)
+                score = self.minimax(next_game, depth - 1, True)[0]
                 if score < v:
                     v = score
                     best_move = move
@@ -339,27 +415,32 @@ class CustomPlayer:
                 player = game.active_player
             else:
                 player = game.inactive_player
-            return self.score(game, player), None
+            return self.score(game, player), (-1, -1)
 
         best_move = (-1, -1)
         legal_moves = game.get_legal_moves()
+
         if maximizing_player:
-            v = -float("inf")
+            v = float("-inf")
             for move in legal_moves:
-                v = max(v, self.alphabeta(game.forecast_move(move), depth - 1, alpha, beta, False)[0])
-                if v >= beta:  # Pruning
-                    return v, move
-                if v > alpha:
-                    alpha = v
+                next_game = game.forecast_move(move)
+                score = self.alphabeta(next_game, depth - 1, alpha, beta, False)[0]
+                if score > v:
+                    v = score
                     best_move = move
-            return alpha, best_move
+                if v >= beta:  # Pruning
+                    return v, best_move
+                alpha = max(alpha, v)
+            return v, best_move
         else:
             v = float("inf")
             for move in legal_moves:
-                v = min(v, self.alphabeta(game.forecast_move(move), depth - 1, alpha, beta, True)[0])
-                if v <= alpha:  # Pruning
-                    return v, move
-                if v < beta:
-                    beta = v
+                next_game = game.forecast_move(move)
+                score = self.alphabeta(next_game, depth - 1, alpha, beta, True)[0]
+                if score < v:
+                    v = score
                     best_move = move
-            return beta, best_move
+                if v <= alpha:  # Pruning
+                    return v, best_move
+                beta = min(beta, v)
+            return v, best_move
